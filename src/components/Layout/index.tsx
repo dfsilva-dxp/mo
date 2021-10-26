22648057e56689ac58004eb4d94c7932d28b629a
Daniel Silva <dani3l.dxp@gmail.com>
2021-10-26 14:54:14 -0300

    feat: add header component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,10 @@
+import { Header } from "../Header";
 import * as S from "./styles";
 
 export const Layout = () => (
   <S.Wrapper>
     <div className="container">
-      <S.Header>Header</S.Header>
+      <Header />
       <S.Sidebar>Aside</S.Sidebar>
       <S.About>About</S.About>
       <S.Content>Content</S.Content>
