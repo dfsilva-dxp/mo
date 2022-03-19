aa08a41bc0e9017a0b6487b63b7a2f101aca4813
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-19 20:12:06 -0300

    chore: add jest e testing-library

---
+++ jest.config.ts
@@ -0,0 +1,32 @@
+export default {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(t|j)sx?$": [
+      "@swc/jest",
+      {
+        jsc: {
+          parser: {
+            syntax: "typescript",
+            tsx: true,
+            decorators: true
+          },
+          keepClassNames: true,
+          transform: {
+            legacyDecorator: true,
+            decoratorMetadata: true,
+            react: {
+              runtime: "automatic"
+            }
+          }
+        },
+        module: {
+          type: "es6",
+          noInterop: true
+        }
+      }
+    ]
+  },
+  setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"]
+};
