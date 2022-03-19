0443c64fe2c427c245c3a0da35e237bfeb9b31ac
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-19 16:52:17 -0300

    initial commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# mo
-
-O Mo. é uma aplicação feita em Reactjs e Typescript para fazer gestão financeira.
--- src/components/Sidebar/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const Sidebar = styled.aside`
-  grid-area: AS;
-  height: 95rem;
-`;
