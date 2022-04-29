d27edc4a335710411594820f4231dff5fa66a83f
Daniel Silva <dani3l.dxp@gmail.com>
2022-04-29 12:19:00 -0300

    delete file

--- src/components/Form/Button/types.ts
+++
@@ -1,9 +0,0 @@
-import { ButtonHTMLAttributes, ReactNode } from "react";
-
-export type ButtonProps = {
-  children?: ReactNode;
-  size?: "sm" | "md" | "lg";
-  fullSize?: boolean;
-  icon?: ReactNode;
-  color?: "primary" | "darkgray" | "lightgray";
-} & ButtonHTMLAttributes<HTMLButtonElement>;
