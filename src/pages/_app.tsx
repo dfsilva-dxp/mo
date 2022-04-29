6b5f3a43c8fcf4f5f6e8df77b0e11520d07622e0
Daniel Silva <dani3l.dxp@gmail.com>
2022-04-29 19:24:46 -0300

    feat: add head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 import { ThemeProvider } from "styled-components";
 
 import { theme } from "styles";
@@ -6,6 +7,21 @@ import { theme } from "styles";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>Mo.app | Gestão financeira</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="shortcut icon"
+          href="/img/icon-512.png"
+          type="image/x-icon"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/img/icon-512.png"
+          type="image/x-icon"
+        />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
